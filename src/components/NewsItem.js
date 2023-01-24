6118671cfdd562209f03a2af84e18839211e2697
ashish <ashishguptaitself@gmail.com>
2023-01-24 23:28:07 +0530

    navabar and news components are added

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        this is NewsItem 2 
+      </div>
+    )
+  }
+}
+
+export default NewsItem
