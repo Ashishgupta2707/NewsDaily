58106672211b1b94511b1f28cf587df6fde365e9
ashish <ashishguptaitself@gmail.com>
2023-01-25 23:52:51 +0530

    Day2 - fetching api keys from news api and add components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description ,imageurl,newsurl } = this.props;
     return (
-      <div>
-        this is NewsItem 2 
+      <div className="my-2">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
